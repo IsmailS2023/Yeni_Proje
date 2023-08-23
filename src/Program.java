174a4a718b4d88a6ee924ef7d41194a8b74fdce2
Ismail sari <ismailsari2023@gmail.com>
2023-08-23 12:19:15 +0300

    Project has been created. ismail added.

--- src/Program.java
+++ src/Program.java
@@ -2,6 +2,7 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("Hello from new project!");
         System.out.println("..........");
+        System.out.println(" new task added");
     }
 
 
